5041231ba3047f1616b2eefbf30e3f603d29ec8a
Portmytech <portmytech@gmail.com>
2021-03-04 09:40:52 +0530

    commit

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ with open('requirements.txt') as f:
 # get version from __version__ variable in bdtheme/__init__.py
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
-with open('theme/__init__.py', 'rb') as f:
+with open('bdtheme/__init__.py', 'rb') as f:
         version = str(ast.literal_eval(_version_re.search(
                 f.read().decode('utf-8')).group(1)))
 
 setup(
-	name='theme',
+	name='sstheme',
 	version=version,
 	description='theme',
 	author='portmytech',
